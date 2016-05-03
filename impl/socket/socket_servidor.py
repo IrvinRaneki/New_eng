845a4c6bb5d8e86140918a8265eac3fdccbb5311
Irvin 'Raneki' Gomes <irvin.r.gomes@gmail.com>
2016-05-03 16:35:49 -0300

    socket agora com plotagem de mensagem recebida

--- impl/socket/socket_servidor.py
+++ impl/socket/socket_servidor.py
@@ -2,7 +2,31 @@
 #!/user/bin/python
 import socket
 
+import matplotlib.pyplot as plt
 import time
+from collections import deque
+import numpy as np
+
+##################################################################
+#                          plota grafico                         #
+##################################################################
+def plota_graf(addr, num):
+    a1 = deque([0]*100)
+    ax = plt.axes(xlim=(0, 100), ylim=(0, 10))
+    d = conect(addr, num)
+
+    line, = plt.plot(a1)
+    plt.ion()
+    plt.ylim([-15,15])
+    plt.show()
+    while num != 'exit':
+        a1.appendleft(next(d))
+        datatoplot = a1.pop()
+        line.set_ydata(a1)
+        plt.draw()
+        print 'recebido: ',a1[0]
+        #time.sleep(0.1)
+        #plt.pause(0.005)
 
 ##################################################################
 #             funcao conect: aguarda o recebimento da msg        #
@@ -12,10 +36,12 @@ def conect(addr, recebe):
     #->mecanismos de recepcao de conexao - argv[1-familia do protocolo() 2-tipo de envio(tcp/ip)]
     serv_socket.bind(addr)
     #->mostra qual o ip e porta o servidor devem aguardar a conexao
-    while True:
+    while recebe!='exit':
         recebe, cliente = serv_socket.recvfrom(1024)
         #->apos conexao ha o aguardo de dado enviado pela rede de ate 1024 Bytes (1 argv -> tamanho do buffer)
-        print "mensagem recebida: "+ recebe
+        #print "mensagem recebida: "+ recebe
+        if recebe != 'exit':
+            yield recebe
     serv_socket.close()
 
 ##################################################################
@@ -27,7 +53,7 @@ def main():
     port = 8888
     addr = (host, port)
 
-    conect(addr, recebe)
+    plota_graf(addr, recebe)
 
 if __name__ == '__main__':
     main()
